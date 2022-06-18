3edb2399a88767530259da85a1a9110ac0175664
krishabbashyal <krishab2002@gmail.com>
2022-06-18 14:41:22 -0500

    Debloated application by removing extra files

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>React weather application</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>React Weather Application</h1>
       </header>
     </div>
   );
