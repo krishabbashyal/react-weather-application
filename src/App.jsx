3d40cf82fdc5737b9ac70d72a073605d9ad64f0e
krishabbashyal <krishab2002@gmail.com>
2022-06-18 16:34:05 -0500

    Hardcoded data for Texarakana as a template for getting page to look how I want

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-
 import './App.css';
 
+async function getData() {
+  let data = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Texarkana&appid=6d4f5ac82e55cbf44a1ff5c499b60765&units=imperial')
+  let response = await data.json()
+  return response
+}
+
+console.log(getData())
+
+
 function App() {
+
+  
+  
+
   return (
     <div className="App">
       <header className="App-header">
-       <h1>React Weather Application</h1>
+       <h2>Texarkana</h2>
+       <h1>96°F</h1>
+       <p>scattered clouds</p>
+       <p>Low: 92°F</p>
+       <p>High: 95°F</p>
+       <p>Feels Like: 105°F</p>
+       <p>Humidity: 50%</p>
+       <h3>{}</h3>
       </header>
     </div>
   );
