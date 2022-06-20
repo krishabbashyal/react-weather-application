14dadbfa3ce028ce1ae6165bb7b3302ebacf8d86
Krishab Bashyal <krishab2002@gmail.com>
2022-06-19 20:53:05 -0500

    made progress with making the api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,14 @@
-import './App.css';
-
-
-let API = process.env.React_App_API_KEY
-
-
-async function getData() {
-  let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Texarkana&appid=${API}&units=imperial`)
-  let response = await data.json()
-  return response
-}
-
-getData().then(response => console.log((response)))
+import React from "react";
+import Weather from "./components/Weather";
+import "./App.css";
 
 
 function App() {
-
-  
-  
-
   return (
-    <div className="App">
-      <header className="App-header">
-       <h2>Texarkana</h2>
-       <h1>96°F</h1>
-       <p>scattered clouds</p>
-       <p>Low: 92°F</p>
-       <p>High: 95°F</p>
-       <p>Feels Like: 105°F</p>
-       <p>Humidity: 50%</p>
-       <h3>{}</h3>
-      </header>
-    </div>
+    <Weather />
   );
 }
 
 export default App;
+
+
---
+++ src/components/Weather.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./Weather.css";
+
+let API = process.env.React_App_API_KEY;
+
+function Weather() {
+  const [weatherData, setWeatherData] = useState([{}])
+  const [city, setCity] = useState("")
+
+
+  const getData = (event) => {
+      if (event.key == "Enter") {
+          fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}&units=imperial`).then(
+              response => response.json().then(data => {setWeatherData(data) 
+                setCity("")})
+          )
+      }
+  }
+
+
+  if (typeof weatherData.main === 'undefined') {
+      return (
+        <input className = "city-input" placeholder="Search another city" onChange = {event => setCity(event.target.value)} value = {city} onKeyPress = {getData} />
+      )
+    
+  } else {
+    return (
+        <div className="App">
+          <input className = "city-input" placeholder="Search another city" onChange = {event => setCity(event.target.value)} value = {city} onKeyPress = {getData} />
+    
+          <header className="city-title">
+            <img src="" alt="" />
+            <h1>{weatherData.name}</h1>
+            <p>Scattered Clouds</p>
+          </header>
+          <header className="current-temp">
+            <img
+              src="http://openweathermap.org/img/wn/03d@2x.png"
+              alt="Scattered Clouds"
+            />
+            <h1>96°</h1>
+          </header>
+          <header className = "additional-temp-info">
+            <p>Low: 92° / High: 95°</p>
+            <p>Feels like 105°F</p>
+          </header>
+          <header className = "additional-weather-info">
+            <p>Humidity: 50%</p>
+            <p>Wind Speed: 11MPH</p>
+            <p>There are 0 clouds in the vicinity</p>
+          </header>
+        </div>
+        );
+    }
+}
+
+export default Weather;
+
+
